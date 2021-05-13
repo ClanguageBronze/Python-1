905fec56bd88d7b89598cb132166479deaee113e
Bronze2 <leedongsu97@naver.com>
2021-05-13 14:43:32 +0900

    add image

---
+++ main.py
@@ -0,0 +1,17 @@
+from tkinter import *
+from tkinter import font
+import tkinter.ttk
+
+class Main:
+    def __init__(self):
+        window=Tk()
+        window.title("공연 전시 정보 검색")
+        frame=Frame(window,width=1200,height=800)
+        frame.pack()
+        self.GmailImage=PhotoImage(file='Image/Gmail.png')
+        button=Button(frame,text='Gmail',image=self.GmailImage)
+        button.place(x=1200-self.GmailImage.width(),y=0)
+        button.pack()
+        window.mainloop()
+
+Main()
