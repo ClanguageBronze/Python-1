928fd08383271adcd4c715b45c16b02f3c2c3f28
Bronze2 <leedongsu97@naver.com>
2021-05-16 21:56:06 +0900

    add Scrollbar

--- Show.py
+++ Show.py
@@ -3,4 +3,10 @@ from xml.dom.minidom import parse, parseString
 class Contents:
     def __init__(self):
         self.Period=http.client.HTTPConnection("culture.go.kr")
-        self.contents.request("GET","/openapi/rest/publicperformancedisplays/period")
+        self.Period.request("GET","/openapi/rest/publicperformancedisplays/period")
+        self.area=http.client.HTTPConnection("culture.go.kr")
+        self.area.request("GET","/openapi/rest/publicperformancedisplays/area")
+        self.realm=http.client.HTTPConnection("culture.go.kr")
+        self.realm.request("GET","/openapi/rest/publicperformancedisplays/realm")
+        self.detail = http.client.HTTPConnection("culture.go.kr")
+        self.detail.request("GET", "/openapi/rest/publicperformancedisplays/d/")
--- main.py
+++ main.py
@@ -6,12 +6,58 @@ class Main:
     def __init__(self):
         window=Tk()
         window.title("공연 전시 정보 검색")
+        global SearchListBox
         frame=Frame(window,width=1200,height=800)
         frame.pack()
         self.GmailImage=PhotoImage(file='Image/Gmail.png')
         button=Button(frame,text='Gmail',image=self.GmailImage)
         button.pack()
-        button.place(x=1200 - self.GmailImage.width(), y=0)
+        button.place(x=1200 - self.GmailImage.width(), y=100)
+        PeriodScrollBar=Scrollbar(window)
+        PeriodScrollBar.pack()
+        PeriodScrollBar.place(x=850,y=120)
+        TempFont= font.Font(window,size=15,weight='bold',family='Consolas')
+        SearchListBox=Listbox(window,font=TempFont,activestyle='none',
+                              width=30,height=2,borderwidth=1,relief='ridge',
+                              yscrollcommand=PeriodScrollBar.set)
+        SearchListBox.pack()
+        SearchListBox.place(x=520,y=120)
+        PeriodScrollBar.config(command=SearchListBox.yview)
+        AreaScrollBar = Scrollbar(window)
+        AreaScrollBar.pack()
+        AreaScrollBar.place(x=850, y=50)
+        TempFont = font.Font(window, size=15, weight='bold', family='Consolas')
+        AreaSearchListBox = Listbox(window, font=TempFont, activestyle='none',
+                                width=30, height=2, borderwidth=1, relief='ridge',
+                                yscrollcommand=AreaScrollBar.set)
+        AreaSearchListBox.pack()
+        AreaSearchListBox.place(x=520, y=50)
+        AreaScrollBar.config(command=AreaSearchListBox.yview)
+
+        ReleamScrollBar = Scrollbar(window)
+        ReleamScrollBar.pack()
+        ReleamScrollBar.place(x=850, y=190)
+        TempFont = font.Font(window, size=15, weight='bold', family='Consolas')
+        ReleamSearchListBox = Listbox(window, font=TempFont, activestyle='none',
+                                    width=30, height=2, borderwidth=1, relief='ridge',
+                                    yscrollcommand=ReleamScrollBar.set)
+        ReleamSearchListBox.pack()
+        ReleamSearchListBox.place(x=520, y=190)
+        ReleamScrollBar.config(command=ReleamSearchListBox.yview)
+
+        SearchResultScrb=Scrollbar(window)
+        SearchResultScrb.pack()
+        SearchResultScrb.place(x=850,y=260)
+        TempFont=font.Font(window,size=10,family='Consolas')
+
+        SearchText=Text(window,width=47,height=5,borderwidth=1,relief='ridge',
+                        yscrollcommand=SearchResultScrb.set)
+        SearchText.pack()
+        SearchText.place(x=520,y=260)
+        SearchResultScrb.config(command=SearchText.yview)
+
+        SearchText.configure(state='disabled')
+
         window.mainloop()
 
 Main()
