94e2012c62a353aa965f2b5ccee436544f943c69
Bronze2 <leedongsu97@naver.com>
2021-05-18 09:38:01 +0900

    add Naverurl and applicate background

--- Show.py
+++ Show.py
@@ -2,6 +2,8 @@ import http.client
 from xml.dom.minidom import parse, parseString
 class Contents:
     def __init__(self):
+        self.client_id="rxUZBymLQbyAVdAs19er"
+        self.client_Secret="rxUZBymLQbyAVdAs19er"
         self.Period=http.client.HTTPConnection("culture.go.kr")
         self.Period.request("GET","/openapi/rest/publicperformancedisplays/period")
         self.area=http.client.HTTPConnection("culture.go.kr")
@@ -10,3 +12,5 @@ class Contents:
         self.realm.request("GET","/openapi/rest/publicperformancedisplays/realm")
         self.detail = http.client.HTTPConnection("culture.go.kr")
         self.detail.request("GET", "/openapi/rest/publicperformancedisplays/d/")
+
+        self.Naver_url="https://openapi.naver.com/v1/search/image.xml?query="
--- main.py
+++ main.py
@@ -6,30 +6,39 @@ class Main:
     def __init__(self):
         window=Tk()
         window.title("공연 전시 정보 검색")
+        self.img=PhotoImage(file="Image/BackGround00.png")
+        self.bg=Canvas(window,width=1200,height=800)
+        self.bg.create_image(0,0,image=self.img,anchor=NW)
+        self.bg.place(x=0,y=0)
+        self.bg.pack()
+        self.logoimg = PhotoImage(file="Image/logo.png")
+        logo = Label(window, image=self.logoimg)
+        logo.pack()
+        logo.place(x=0, y=0)
         global SearchListBox
-        frame=Frame(window,width=1200,height=800)
-        frame.pack()
         self.GmailImage=PhotoImage(file='Image/Gmail.png')
-        button=Button(frame,text='Gmail',image=self.GmailImage)
+        button=Button(window,text='Gmail',image=self.GmailImage)
         button.pack()
         button.place(x=1200 - self.GmailImage.width(), y=100)
         PeriodScrollBar=Scrollbar(window)
         PeriodScrollBar.pack()
         PeriodScrollBar.place(x=850,y=120)
-        TempFont= font.Font(window,size=15,weight='bold',family='Consolas')
+        TempFont= font.Font(window,size=30,weight='bold',family='Consolas')
         SearchListBox=Listbox(window,font=TempFont,activestyle='none',
-                              width=30,height=2,borderwidth=1,relief='ridge',
+                              width=15,height=1,borderwidth=1,relief='ridge',
                               yscrollcommand=PeriodScrollBar.set)
+        SearchListBox.insert(1,"기간별 공연 검색")
         SearchListBox.pack()
         SearchListBox.place(x=520,y=120)
         PeriodScrollBar.config(command=SearchListBox.yview)
         AreaScrollBar = Scrollbar(window)
         AreaScrollBar.pack()
         AreaScrollBar.place(x=850, y=50)
-        TempFont = font.Font(window, size=15, weight='bold', family='Consolas')
+        TempFont = font.Font(window, size=30, weight='bold', family='Consolas')
         AreaSearchListBox = Listbox(window, font=TempFont, activestyle='none',
-                                width=30, height=2, borderwidth=1, relief='ridge',
+                                width=15, height=1, borderwidth=1, relief='ridge',
                                 yscrollcommand=AreaScrollBar.set)
+        AreaSearchListBox.insert(1, "지역별 공연 검색")
         AreaSearchListBox.pack()
         AreaSearchListBox.place(x=520, y=50)
         AreaScrollBar.config(command=AreaSearchListBox.yview)
@@ -37,10 +46,11 @@ class Main:
         ReleamScrollBar = Scrollbar(window)
         ReleamScrollBar.pack()
         ReleamScrollBar.place(x=850, y=190)
-        TempFont = font.Font(window, size=15, weight='bold', family='Consolas')
+        TempFont = font.Font(window, size=30, weight='bold', family='Consolas')
         ReleamSearchListBox = Listbox(window, font=TempFont, activestyle='none',
-                                    width=30, height=2, borderwidth=1, relief='ridge',
+                                    width=15, height=1, borderwidth=1, relief='ridge',
                                     yscrollcommand=ReleamScrollBar.set)
+        ReleamSearchListBox.insert(1, "분야별 공연 검색")
         ReleamSearchListBox.pack()
         ReleamSearchListBox.place(x=520, y=190)
         ReleamScrollBar.config(command=ReleamSearchListBox.yview)
@@ -50,7 +60,7 @@ class Main:
         SearchResultScrb.place(x=850,y=260)
         TempFont=font.Font(window,size=10,family='Consolas')
 
-        SearchText=Text(window,width=47,height=5,borderwidth=1,relief='ridge',
+        SearchText=Text(window,font=TempFont,width=47,height=5,borderwidth=1,relief='ridge',
                         yscrollcommand=SearchResultScrb.set)
         SearchText.pack()
         SearchText.place(x=520,y=260)
